cb50a40744a5a06e2d347f0a957482a8c90419b8
Jesse Willig <jessewillig@outlook.com>
2021-01-31 19:46:13 -0800

    completed current weather function:
    :q!

--- assets/script.js
+++ assets/script.js
@@ -13,5 +13,22 @@ function currentWeather () {
         method: "GET"
     }).then(function (response) {
         console.log(response);
-    })
+        $("#today").append(cityTag);
+        // current city name
+        var currentCity = response.name;
+        var cityTag = $("<h1>").text(currentCity);
+        $("#today").append(cityTag);
+        // current temperature
+        var currentTemp = response.main.temp;
+        var temperatureTag = $("<h5>").text("Temperature: " + currentTemp);
+        $("#today").append(temperatureTag);
+        console.log(currentTemp);
+        // wind speed
+        var currentWind = response.wind.speed;
+        var windTag = $("<h5>").text("Wind Speed: " + currentWind);
+        $("#today").append(windTag);
+        // humidity
+        var currentHumidity = response.main.humidity;
+        var humidityTag = $("<h5>").text("Humidity: " + currentHumidity);
+    });
 };
