0f520457f1c536c2c942b045667058dbdc683e59
Jesse Willig <jessewillig@outlook.com>
2021-02-10 19:40:29 -0800

    added F and % and MPH

--- assets/script.js
+++ assets/script.js
@@ -26,16 +26,16 @@ function currentWeather (cityName) {
         console.log(cityTag);
         // current temperature
         var currentTemp = response.main.temp;
-        var temperatureTag = $("<h5>").text("Temperature: " + currentTemp);
+        var temperatureTag = $("<h5>").text("Temperature: " + currentTemp + " F");
         $("#today").append(temperatureTag);
         console.log(currentTemp);
         // wind speed
         var currentWind = response.wind.speed;
-        var windTag = $("<h5>").text("Wind Speed: " + currentWind);
+        var windTag = $("<h5>").text("Wind Speed: " + currentWind + " MPH");
         $("#today").append(windTag);
         // humidity
         var currentHumidity = response.main.humidity;
-        var humidityTag = $("<h5>").text("Humidity: " + currentHumidity);
+        var humidityTag = $("<h5>").text("Humidity: " + currentHumidity + "%");
         $("#today").append(humidityTag);
     });
 };
@@ -54,51 +54,77 @@ function forecast (cityName) {
         var day1 = $("#day1").text("Date: " + response.list[0].dt_txt);
         $("#day1").append(day1);
 
-        var temperature1 = $(".currentTemperature1").text("Temperature: " + response.list[0].main.temp);
+        var temperature1 = $(".currentTemperature1").text("Temperature: " + response.list[0].main.temp + " F");
         $(".currentTemperature1").append(temperature1);
 
-        var humidity1 = $(".currentHumidity1").text("Humidity: " + response.list[0].main.humidity);
+        var humidity1 = $(".currentHumidity1").text("Humidity: " + response.list[0].main.humidity + "%");
         $(".currentHumidity1").append(humidity1);
 
         // forcast day 2 of 5, date, temp, humidity
         var day2 = $("#day2").text("Date: " + response.list[9].dt_txt);
         $("#day2").append(day2);
 
-        var temperature2 = $(".currentTemperature2").text("Temperature: " + response.list[9].main.temp);
+        var temperature2 = $(".currentTemperature2").text("Temperature: " + response.list[9].main.temp + " F");
         $(".currentTemperature2").append(temperature2);
 
-        var humidity2 = $(".currentHumidity2").text("Humidity: " + response.list[9].main.humidity);
+        var humidity2 = $(".currentHumidity2").text("Humidity: " + response.list[9].main.humidity + "%");
         $(".currentHumidity2").append(humidity2);
 
         // forcast day 3 of 5, date, temp, humidity
         var day3 = $("#day3").text("Date: " + response.list[17].dt_txt);
         $("#day3").append(day3);
 
-        var temperature3 = $(".currentTemperature3").text("Temperature: " + response.list[17].main.temp);
+        var temperature3 = $(".currentTemperature3").text("Temperature: " + response.list[17].main.temp + " F");
         $(".currentTemperature3").append(temperature3);
 
-        var humidity3 = $(".currentHumidity3").text("Humidity: " + response.list[17].main.humidity);
+        var humidity3 = $(".currentHumidity3").text("Humidity: " + response.list[17].main.humidity + "%");
         $(".currentHumidity3").append(humidity3);
 
         // forcast day 4 of 5, date, temp, humidity
         var day4 = $("#day4").text("Date: " + response.list[25].dt_txt);
         $("#day4").append(day4);
 
-        var temperature4 = $(".currentTemperature4").text("Temperature: " + response.list[25].main.temp);
+        var temperature4 = $(".currentTemperature4").text("Temperature: " + response.list[25].main.temp + " F");
         $(".currentTemperature4").append(temperature4);
 
-        var humidity4 = $(".currentHumidity4").text("Humidity: " + response.list[25].main.humidity);
+        var humidity4 = $(".currentHumidity4").text("Humidity: " + response.list[25].main.humidity + "%");
         $(".currentHumidity4").append(humidity4);
 
         // forcast day 5 of 5, date, temp, humidity
         var day5 = $("#day5").text("Date: " + response.list[33].dt_txt);
         $("#day5").append(day5);
 
-        var temperature5 = $(".currentTemperature5").text("Temperature: " + response.list[33].main.temp);
+        var temperature5 = $(".currentTemperature5").text("Temperature: " + response.list[33].main.temp + " F");
         $(".currentTemperature5").append(temperature5);
 
-        var humidity5 = $(".currentHumidity5").text("Humidity: " + response.list[33].main.humidity);
+        var humidity5 = $(".currentHumidity5").text("Humidity: " + response.list[33].main.humidity + "%");
         $(".currentHumidity5").append(humidity5);
+
+        // ajax inside ajax for symbol
+        var symbolUrl = "https://openweathermap.org/img/wn/" + response.list[1].weather[0].icon + ".png"
+
+        $.ajax({
+            url: symbolUrl,
+            method: "GET"
+        }).then(function(response1){
+            console.log(response);
+            $(".symbol1").empty()
+            // add image tag with symbol from API to all 5 cards
+            var newImg1 = $(`<img src="${symbolUrl}">`);
+            $(".symbol1").append(newImg1);
+            symbolUrl = "https://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + ".png"
+            var newImg2 = $(`<img src="${symbolUrl}">`);
+            $(".symbol2").append(newImg2);
+            symbolUrl = "https://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + ".png"
+            var newImg3 = $(`<img src="${symbolUrl}">`);
+            $(".symbol3").append(newImg3);
+            symbolUrl = "https://openweathermap.org/img/wn/" + response.list[24].weather[0].icon + ".png"
+            var newImg4 = $(`<img src="${symbolUrl}">`);
+            $(".symbol4").append(newImg4);
+            symbolUrl = "https://openweathermap.org/img/wn/" + response.list[32].weather[0].icon + ".png"
+            var newImg5 = $(`<img src="${symbolUrl}">`);
+            $(".symbol5").append(newImg5);
+        });  
     });
 };
 
